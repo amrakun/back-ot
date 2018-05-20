1955716de5eaddb587db1ee8b92c0a993f3fdb34
batamar <batamar.b@nmtec.co>
2018-05-20 16:38:53 +0800

    Fixed success feedback templates

--- src/data/resolvers/mutations/feedbackResponses.js
+++ src/data/resolvers/mutations/feedbackResponses.js
@@ -1,4 +1,4 @@
-import { Users, Feedbacks, FeedbackResponses } from '../../../db/models';
+import { Users, Companies, Feedbacks, FeedbackResponses } from '../../../db/models';
 import { sendConfigEmail } from '../../../data/utils';
 import { moduleRequireSupplier } from '../../permissions';
 
@@ -10,6 +10,8 @@ const feedbackResponseMutations = {
    */
   async feedbackResponsesAdd(root, doc) {
     const response = await FeedbackResponses.createFeedbackResponse(doc);
+
+    const supplier = await Companies.findOne({ _id: response.supplierId });
     const feedback = await Feedbacks.findOne({ _id: response.feedbackId });
     const createdUser = await Users.findOne({ _id: feedback.createdUserId });
 
@@ -17,6 +19,11 @@ const feedbackResponseMutations = {
       name: 'successFeedbackTemplates',
       kind: 'buyer__new',
       toEmails: [createdUser.email],
+      replacer: text => {
+        return text
+          .replace('{supplier.name}', supplier.basicInfo.enName)
+          .replace('{date}', response.createdDate);
+      },
     });
 
     return response;
