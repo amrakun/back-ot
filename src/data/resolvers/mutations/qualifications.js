41bb3728d4044a0b0d3eac1893ce92931e063baa
batamar <batamar.b@nmtec.co>
2018-09-27 17:50:40 +0800

    Added replacer in prequalification email

--- src/data/resolvers/mutations/qualifications.js
+++ src/data/resolvers/mutations/qualifications.js
@@ -18,11 +18,59 @@ const qualificationMutations = {
 
     await SuppliersByProductCodeLogs.createLog(supplier);
 
+    const basicInfo = supplier.basicInfo || {};
+    const qualification = await Qualifications.findOne({ supplierId });
+
+    const replacer = text => {
+      let failedSectionsEn = '';
+      let failedSectionsMn = '';
+
+      const collect = (status, enText, mnText) => {
+        if (!status) {
+          failedSectionsEn = failedSectionsEn ? `${failedSectionsEn}, ${enText}` : enText;
+          failedSectionsMn = failedSectionsMn ? `${failedSectionsMn}, ${mnText}` : mnText;
+        }
+      };
+
+      if (qualified === false && qualification) {
+        // financial info
+        const isFIPassed = Qualifications.isSectionPassed(qualification.financialInfo);
+
+        // Business info
+        const isBIPassed = Qualifications.isSectionPassed(qualification.businessInfo);
+
+        // Environmental Management
+        const isEIPassed = Qualifications.isSectionPassed(qualification.environmentalInfo);
+
+        // Health & Safety Management System
+        const isHIPassed = Qualifications.isSectionPassed(qualification.healthInfo);
+
+        collect(isFIPassed, 'Financial information', 'Санхүүгийн мэдээлэл');
+        collect(isBIPassed, 'Business integrity & human resource', 'Хүний нөөцийн удирдлага');
+        collect(isEIPassed, 'Environmental management', 'Байгаль орчны удирдлага');
+        collect(
+          isHIPassed,
+          'Health & safety management system',
+          'Эрүүл мэнд, аюулгүй ажиллагааны удирдлагын систем ',
+        );
+      }
+
+      return text
+        .replace('{supplier.enName}', basicInfo.enName || '')
+        .replace('{supplier.mnName}', basicInfo.mnName || '')
+        .replace('{supplier.vendorNumber}', basicInfo.sapNumber || '')
+        .replace('{supplier.tierType}', supplier.tierTypeDisplay())
+        .replace('{failedSectionsEn}', failedSectionsEn)
+        .replace('{failedSectionsMn}', failedSectionsMn)
+        .replace('{supplier._id}', supplier._id);
+    };
+
     // send notification email
     await sendConfigEmail({
       name: 'prequalificationTemplates',
       kind: `supplier__${qualified ? 'qualified' : 'failed'}`,
       toEmails: [supplier.basicInfo.email],
+      replacer,
     });
 
     return Qualifications.prequalify(supplierId, qualified);
