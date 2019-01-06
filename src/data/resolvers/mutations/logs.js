5e03daf0281ebe0514f127d32c7fb4cf0cfebf7f
batamar <batamar.b@nmtec.co>
2019-01-06 18:43:26 +0800

    Used requireLogin in logsWrite

--- src/data/resolvers/mutations/logs.js
+++ src/data/resolvers/mutations/logs.js
@@ -1,13 +1,12 @@
 import { ActivityLogs } from '../../../db/models';
+import { requireLogin } from '../../permissions';
 
 const logMutations = {
   logsWrite(root, { apiCall }, { user }) {
-    if (!user) {
-      return;
-    }
-
     return ActivityLogs.write({ apiCall: apiCall.substring(1), userId: user._id });
   },
 };
+
+requireLogin(logMutations, 'logsWrite');
 
 export default logMutations;
