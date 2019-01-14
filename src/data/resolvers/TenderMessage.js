49532731e6f0ce3c485bd682d80c79863a7ef8d8
Dulguun Otgon <dulguun0225@gmail.com>
2019-01-14 18:52:18 +0800

    fix

--- src/data/resolvers/TenderMessage.js
+++ src/data/resolvers/TenderMessage.js
@@ -10,7 +10,7 @@ export default {
   },
 
   recipientSuppliers({ recipientSupplierIds }) {
-    return Users.findOne({ _id: { $in: recipientSupplierIds } });
+    return Users.find({ _id: { $in: recipientSupplierIds } });
   },
 
   senderSupplier({ senderSupplierId }) {
