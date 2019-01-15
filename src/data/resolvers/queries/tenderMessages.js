85b38f5229fa45401131f550b4b413740458f78f
Dulguun Otgon <dulguun0225@gmail.com>
2019-01-15 14:59:40 +0800

    name correction

--- src/data/resolvers/queries/tenderMessages.js
+++ src/data/resolvers/queries/tenderMessages.js
@@ -1,9 +1,9 @@
-import { TenderMessage } from '../../../db/models';
+import { TenderMessages } from '../../../db/models';
 import { moduleRequireLogin } from '../../permissions';
 
 const TenderMessageQuery = {
   async tenderMessages(root, { tenderId, page = 1, perPage = 20 }, { user }) {
-    const docs = TenderMessage.find({
+    const docs = TenderMessages.find({
       tenderId,
       $or: [
         { senderBuyerId: user._id },
@@ -18,7 +18,7 @@ const TenderMessageQuery = {
     return docs;
   },
   async tenderMessageDetail(root, { _id }, { user }) {
-    return TenderMessage.findOne({
+    return TenderMessages.findOne({
       _id,
       $or: [
         { senderBuyerId: user._id },
