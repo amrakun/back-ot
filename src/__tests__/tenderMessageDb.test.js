164fad0204ccc62f18b070d7cee4d732ada68272
Dulguun Otgon <dulguun0225@gmail.com>
2019-01-29 03:30:51 -0500

    test fix

--- src/__tests__/tenderMessageDb.test.js
+++ src/__tests__/tenderMessageDb.test.js
@@ -25,8 +25,7 @@ describe('TenderMessage db', () => {
     const supplier2 = await userFactory({ isSupplier: true });
     const tender = await tenderFactory();
 
-    await mutations.tenderMessageBuyerSend(
-      {},
+    await TenderMessages.tenderMessageBuyerSend(
       {
         tenderId: tender._id,
         subject: 'attach',
@@ -34,7 +33,7 @@ describe('TenderMessage db', () => {
         recipientSupplierIds: [supplier1.companyId],
         attachment: { url: 'attach10.png', name: '/attach10' },
       },
-      { user: admin },
+      admin,
     );
 
     expect(await TenderMessages.isAuthorizedToDownload('attach10.png', admin)).toBe(true);
