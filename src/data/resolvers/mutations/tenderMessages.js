a8c70035db244f086d73c929c63089e2c291582c
Dulguun Otgon <dulguun0225@gmail.com>
2019-01-29 14:37:20 +0800

    email message notification

--- src/data/resolvers/mutations/tenderMessages.js
+++ src/data/resolvers/mutations/tenderMessages.js
@@ -1,14 +1,38 @@
-import { TenderMessages } from '../../../db/models';
+import { TenderMessages, Tenders } from '../../../db/models';
 
 import { requireSupplier, requireBuyer } from '../../permissions';
+import { sendEmailToSuppliers, sendEmailToBuyer } from '../../tenderUtils';
+
+//supplier__message__notification
+//buyer__message__notification
 
 const tenderMessageMutations = {
   async tenderMessageBuyerSend(parent, args, { user }) {
-    return TenderMessages.tenderMessageBuyerSend(args, user);
+    const p = await TenderMessages.tenderMessageBuyerSend(args, user);
+    const tender = await Tenders.findOne({ _id: args.tenderId });
+    const supplierIds = args.recipientSupplierIds;
+    try {
+      await sendEmailToSuppliers({
+        kind: 'message__notification',
+        tender,
+        supplierIds,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+    return p;
   },
 
   async tenderMessageSupplierSend(parent, args, { user }) {
-    return TenderMessages.tenderMessageSupplierSend(args, user);
+    const p = await TenderMessages.tenderMessageSupplierSend(args, user);
+    const tender = await Tenders.findOne({ _id: args.tenderId });
+    try {
+      await sendEmailToBuyer({ kind: 'message__notification', tender });
+    } catch (e) {
+      console.log(e);
+    }
+    return p;
   },
 
   async tenderMessageSetAsRead(parent, args, { user }) {
