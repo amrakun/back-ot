9a70269c279f4b8dc69fe58af0d2876bd134a26b
batamar <batamar.b@nmtec.co>
2019-02-08 01:41:22 +0800

    Resetted startup.js

--- src/startup.js
+++ src/startup.js
@@ -1,1 +1,14 @@
-export const init = () => {};
+import dotenv from 'dotenv';
+import './cronJobs/companies';
+import './cronJobs/audits';
+import './cronJobs/tenders';
+import './cronJobs/feedbacks';
+import './cronJobs/blockedCompanies';
+
+import { trackSes } from './trackers/sesTracker';
+
+dotenv.config();
+
+export const init = async app => {
+  trackSes(app);
+};
