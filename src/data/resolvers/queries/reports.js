26793619ecb14d4d81d2882925a01aea94ec7485
batamar <batamar.b@nmtec.co>
2019-02-13 12:57:48 +0800

    Fixed tender report

--- src/data/resolvers/queries/reports.js
+++ src/data/resolvers/queries/reports.js
@@ -146,8 +146,10 @@ const reportsQuery = {
       sheet.cell(rowIndex, 2).value(it.number);
       sheet.cell(rowIndex, 3).value(it.name || '');
 
+      const supplierIds = await it.getExactSupplierIds();
+
       const suppliers = await Companies.find(
-        { _id: { $in: it.getSupplierIds() } },
+        { _id: { $in: supplierIds } },
         { basicInfo: 1 },
       );
 
