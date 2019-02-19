9e40d4fa53c10cf7267420b94f8c170155928695
batamar <batamar.b@nmtec.co>
2019-02-19 13:17:44 +0800

    Fixed user list pagination

--- src/data/resolvers/queries/users.js
+++ src/data/resolvers/queries/users.js
@@ -2,6 +2,22 @@ import { Users } from '../../../db/models';
 import { requireBuyer } from '../../permissions';
 import { paginate } from './utils';
 
+const listFilterSelector = (args) => {
+  const { search } = args;
+  const selector = { isSupplier: false, isActive: true };
+
+  if (search) {
+    selector.$or = [
+      { firstName: new RegExp(`.*${search}.*`, 'i') },
+      { lastName: new RegExp(`.*${search}.*`, 'i') },
+      { username: new RegExp(`.*${search}.*`, 'i') },
+      { email: new RegExp(`.*${search}.*`, 'i') },
+    ];
+  }
+
+  return selector;
+}
+
 const userQueries = {
   /**
    * Users list
@@ -9,19 +25,7 @@ const userQueries = {
    * @return {Promise} sorted and filtered users objects
    */
   users(root, args) {
-    const { search } = args;
-    const selector = { isSupplier: false, isActive: true };
-
-    if (search) {
-      selector.$or = [
-        { firstName: new RegExp(`.*${search}.*`, 'i') },
-        { lastName: new RegExp(`.*${search}.*`, 'i') },
-        { username: new RegExp(`.*${search}.*`, 'i') },
-        { email: new RegExp(`.*${search}.*`, 'i') },
-      ];
-    }
-
-    const users = paginate(Users.find(selector), args);
+    const users = paginate(Users.find(listFilterSelector(args)), args);
 
     return users.sort({ username: 1 });
   },
@@ -44,8 +48,8 @@ const userQueries = {
    * @param {Object} object3.user - User making this request
    * @return {Promise} total count
    */
-  usersTotalCount() {
-    return Users.find({ isSupplier: false, isActive: true }).count();
+  usersTotalCount(root, args) {
+    return Users.find(listFilterSelector(args)).count();
   },
 
   /**
