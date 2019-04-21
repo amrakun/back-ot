14f11ce2efc820f51232b50121e5dd888bdae071
batamar <batamar.b@nmtec.co>
2019-04-21 23:23:17 +0800

    Added buyer company id field migration

---
+++ migrations/1545048509399-fix-buyer-company-id-field.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { Users, Companies } from '../src/db/models';
+
+dotenv.config();
+
+/**
+ * Added rfqType field
+ */
+module.exports.up = next => {
+  const { MONGO_URL = '' } = process.env;
+
+  mongoose.connect(
+    MONGO_URL,
+    async () => {
+      const users = await Users.find({ isSupplier: false, companyId: { $exists: true } });
+
+      for (const user of users) {
+          const company = await Companies.findOne({ _id: user.companyId });
+
+          await Users.update({ _id: user._id }, { $set: { companyId: null } });
+
+          console.log(user.email, company.basicInfo);
+      }
+
+      next();
+    },
+  );
+};
