24ac60bdc9d6aa6efaccdaa202d003b1f2a9e99e
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-08-08 00:43:33 +0800

    add log related schema and queries

--- src/data/schema/log.js
+++ src/data/schema/log.js
@@ -8,6 +8,31 @@ export const types = `
     description: String!
     createdAt:   Date!
   }
+
+  type Log {
+    _id: String
+    createdAt: Date
+    createdBy: String
+    type: String
+    action: String
+    objectId: String
+    unicode: String
+    description: String
+    addedData: String
+    removedData: String
+    changedData: String
+    unchangedData: String
+  }
+
+  type LogList {
+    logs: [Log]
+    totalCount: Int
+  }
+
+  type Field {
+    name: String
+    label: String
+  }
 `;
 
 export const queries = `
@@ -18,11 +43,26 @@ export const queries = `
   logsSearchesPerBuyerExport(startDate: Date!, endDate: Date!): String
   logsEoiCreatedAndSentExport(startDate: Date!, endDate: Date!): String
   logsRfqCreatedAndSentExport(startDate: Date!, endDate: Date!): String
-  logsSuppliersByProductCodeLogsExport(startDate: Date!, endDate: Date!, productCodes: [String!]): String
+  logsSuppliersByProductCodeLogsExport(
+    startDate: Date!,
+    endDate: Date!,
+    productCodes: [String!]
+  ): String
   logsActivityLogsExport(startDate: Date!, endDate: Date!, module: String): String
 
   logsTender(page: Int, perPage: Int, tenderId: String): [TenderLog]
   logsTenderTotalCount(tenderId: String): Int
+
+  logs(
+    start: String,
+    end: String,
+    userId: String,
+    action: String,
+    page: Int,
+    perPage: Int
+  ): LogList
+
+  getDbFieldLabels(type: String): [Field]
 `;
 
 export const mutations = `
