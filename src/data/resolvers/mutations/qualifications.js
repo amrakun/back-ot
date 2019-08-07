0f4c18d1b44b710c5a42ae7e45b4a66a7baa6200
Buyantogtokh <buyantogtokh.s@nmma.co>
2019-08-08 00:54:08 +0800

    enable log writing in qualification mutations

--- src/data/resolvers/mutations/qualifications.js
+++ src/data/resolvers/mutations/qualifications.js
@@ -1,5 +1,5 @@
 import { Qualifications, Companies, SuppliersByProductCodeLogs } from '../../../db/models';
-import { sendConfigEmail } from '../../../data/utils';
+import { sendConfigEmail, putUpdateLog } from '../../../data/utils';
 import { requireBuyer } from '../../permissions';
 
 export const generateReplacer = async (supplier, qualified) => {
@@ -63,18 +63,50 @@ export const generateReplacer = async (supplier, qualified) => {
   return replacer;
 };
 
+/**
+ * @param {string} supplierId Supplier id
+ * @returns Company name
+ */
+const getCompanyName = async supplierId => {
+  let companyName = supplierId;
+  const company = await Companies.findOne({ _id: supplierId });
+
+  if (company && company.basicInfo && company.basicInfo.enName) {
+    companyName = company.basicInfo.enName;
+  }
+
+  return companyName;
+};
+
 const qualificationMutations = {
   /*
    * Save tier type
    */
-  qualificationsSaveTierType(root, { supplierId, tierType }) {
-    return Qualifications.saveTierType(supplierId, 'tierType', tierType);
+  async qualificationsSaveTierType(root, { supplierId, tierType }, { user }) {
+    const qualification = await Qualifications.findOne({ supplierId });
+    const updated = await Qualifications.saveTierType(supplierId, 'tierType', tierType);
+
+    if (qualification) {
+      const companyName = await getCompanyName(supplierId);
+
+      await putUpdateLog(
+        {
+          type: 'qualification',
+          object: { tierType: qualification.tierType },
+          newData: JSON.stringify({ tierType }),
+          description: `Tier type for company "${companyName}" has been changed`,
+        },
+        user,
+      );
+    }
+
+    return updated;
   },
 
   /*
    * Prequalify a supplier
    */
-  async qualificationsPrequalify(root, { supplierId, qualified, templateObject }) {
+  async qualificationsPrequalify(root, { supplierId, qualified, templateObject }, { user }) {
     const supplier = await Companies.findOne({ _id: supplierId });
 
     await SuppliersByProductCodeLogs.createLog(supplier);
@@ -92,7 +124,30 @@ const qualificationMutations = {
       replacer,
     });
 
-    return Qualifications.prequalify(supplierId, qualified);
+    const oldInfo = {
+      isPrequalified: supplier.isPrequalified,
+      prequalifiedDate: supplier.prequalifiedDate,
+      isPrequalificationInfoEditable: supplier.isPrequalificationInfoEditable,
+    };
+    const prequalified = await Qualifications.prequalify(supplierId, qualified);
+    const logDoc = {
+      isPrequalified: qualified,
+      prequalifiedDate: new Date(),
+      isPrequalificationInfoEditable: false,
+    };
+    const companyName = await getCompanyName(supplierId);
+
+    await putUpdateLog(
+      {
+        type: 'qualification',
+        object: oldInfo,
+        newData: JSON.stringify(logDoc),
+        description: `Prequalification status for company "${companyName}" has been changed`,
+      },
+      user,
+    );
+
+    return prequalified;
   },
 };
 
@@ -101,13 +156,29 @@ const sections = ['financial', 'business', 'environmental', 'health'];
 sections.forEach(section => {
   // capitalize first letter
   const capsedName = section.charAt(0).toUpperCase() + section.slice(1);
-
   const name = `qualificationsSave${capsedName}Info`;
   const sectionName = `${section}Info`;
 
-  qualificationMutations[name] = (root, args) => {
-    const value = args[`${section}Info`];
-    return Qualifications.updateSection(args.supplierId, sectionName, value);
+  qualificationMutations[name] = async (root, args, { user }) => {
+    const value = args[sectionName];
+    const qualification = await Qualifications.findOne({ supplierId: args.supplierId });
+    const updated = await Qualifications.updateSection(args.supplierId, sectionName, value);
+
+    if (qualification && updated) {
+      const companyName = await getCompanyName(args.supplierId);
+
+      await putUpdateLog(
+        {
+          type: 'qualification',
+          object: qualification[sectionName] || {},
+          newData: JSON.stringify(value),
+          description: `Qualification for "${companyName}" has been edited`,
+        },
+        user,
+      );
+    }
+
+    return updated;
   };
 
   requireBuyer(qualificationMutations, name);
