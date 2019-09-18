d52fb6f8b2c89a83ae04ac2856868cc04ab0392b
BatAmar Battulga <batamar.b@nmtec.co>
2019-09-18 08:30:50 +0800

    extracted crons

---
+++ src/cronJobs/index.js
@@ -0,0 +1,38 @@
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import { connect } from '../db/connection';
+
+import './companies';
+import './audits';
+import './tenders';
+import './feedbacks';
+import './blockedCompanies';
+
+// load environment variables
+dotenv.config();
+
+// connect to mongo database
+connect();
+
+const app = express();
+
+// for health check
+app.get('/status', async (req, res) => {
+  res.end('ok');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Error handling middleware
+app.use((error, req, res) => {
+  console.error(error.stack);
+  res.status(500).send(error.message);
+});
+
+const { PORT_CRONS } = process.env;
+
+app.listen(PORT_CRONS, () => {
+  console.log(`Cron Server is now running on ${PORT_CRONS}`);
+});
--- src/startup.js
+++ src/startup.js
@@ -1,9 +1,4 @@
 import dotenv from 'dotenv';
-import './cronJobs/companies';
-import './cronJobs/audits';
-import './cronJobs/tenders';
-import './cronJobs/feedbacks';
-import './cronJobs/blockedCompanies';
 
 import { trackSes } from './trackers/sesTracker';
 
