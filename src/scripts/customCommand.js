fc924706e492dcaa1035d87d5329ffc25621eae8
BatAmar Battulga <batamar.b@nmtec.co>
2019-12-15 23:11:42 +0800

    update customCommand

--- src/scripts/customCommand.js
+++ src/scripts/customCommand.js
@@ -1,88 +1,83 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { uniq } from 'underscore';
-import { Tenders, TenderResponses, Companies } from '../db/models';
+import { Tenders, TenderResponses } from '../db/models';
 
 dotenv.config();
 
 mongoose.Promise = global.Promise;
 
-export const customCommand = async () => {
+const command = async next => {
   const { MONGO_URL = '' } = process.env;
 
   await mongoose.connect(MONGO_URL);
 
-  const tenders = await Tenders.find({
-    createdDate: { $gt: new Date('2019-11-01') },
-    status: 'closed',
-  });
+  const tenders = await Tenders.find({});
 
-  let totalInvalid = 0;
-  let totalInvalidWithUniqueCodes = 0;
-  let completeInvalidTenders = [];
+  // const tenders = await Tenders.find({
+  //   createdDate: { $gt: new Date('2019-11-01') },
+  //   status: 'closed',
+  // });
+
+  let allInvalid = 0;
+  let withUniqueCodes = 0;
 
   for (const tender of tenders) {
     const requestedProducts = tender.requestedProducts || [];
-    const requestedProductsUniqueCodes = uniq(
-      requestedProducts.map(rp => rp.purchaseRequestNumber),
-    );
+
+    if (requestedProducts.length === 0) {
+      continue;
+    }
+
+    const requestedProductsUniqueCodes = uniq(requestedProducts.map(rp => rp.code));
     const isAllProductsHaveUniqueCode =
       requestedProducts.length === requestedProductsUniqueCodes.length;
 
-    if (requestedProducts.length > 0) {
-      let status = 'valid';
+    let status = 'valid';
 
-      const responses = await TenderResponses.find({ tenderId: tender._id });
+    const responses = await TenderResponses.find({ tenderId: tender._id });
 
-      for (const response of responses) {
-        const respondedProducts = response.respondedProducts || [];
+    for (const response of responses) {
+      const respondedProducts = response.respondedProducts || [];
 
-        if (respondedProducts.length > 0 && requestedProducts.length !== respondedProducts.length) {
-          status = 'invalid';
-        }
+      if (respondedProducts.length > 0 && requestedProducts.length !== respondedProducts.length) {
+        status = 'invalid';
       }
+    }
 
-      if (status === 'invalid') {
-        totalInvalid++;
+    if (status === 'invalid') {
+      allInvalid++;
 
-        if (isAllProductsHaveUniqueCode) {
-          totalInvalidWithUniqueCodes++;
-        } else {
-          completeInvalidTenders.push(tender);
+      if (isAllProductsHaveUniqueCode) {
+        for (const response of responses) {
+          const respondedProducts = response.respondedProducts || [];
+          const fixedRespondedProducts = [];
+
+          for (const respondedProduct of respondedProducts) {
+            const requestedProduct = requestedProducts.find(
+              rp => rp.code === respondedProduct.code,
+            );
+
+            fixedRespondedProducts.push({
+              ...respondedProduct.toObject(),
+              id: requestedProduct.id,
+            });
+          }
+
+          // await TenderResponses.update(
+          //   { _id: response._id },
+          //   { $set: { respondedProducts: fixedRespondedProducts } },
+          // );
         }
+
+        withUniqueCodes++;
       }
     }
   }
 
-  console.log(totalInvalid, totalInvalidWithUniqueCodes);
-
-  // for (const tender of completeInvalidTenders) {
-  //   const requestedProducts = tender.requestedProducts || [];
-
-  //   console.log(
-  //     `Requested products ========================== ${tender.number} ${JSON.stringify(
-  //       requestedProducts,
-  //     )}`,
-  //   );
-
-  //   const responses = await TenderResponses.find({ tenderId: tender._id });
-
-  //   for (const response of responses) {
-  //     if (response.respondedProducts.length === 0) {
-  //       continue;
-  //     }
-
-  //     if (response.respondedProducts.length !== requestedProducts.length) {
-  //       console.log(
-  //         `Invalid response ========================================: ${JSON.stringify(
-  //           response.respondedProducts,
-  //         )}`,
-  //       );
-  //     }
-  //   }
-  // }
+  console.log('All invalid: ', allInvalid, 'With unique codes: ', withUniqueCodes);
 
   mongoose.connection.close();
 };
 
-customCommand();
+command();
