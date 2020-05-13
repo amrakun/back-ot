f7f93d71ffcf7f5e71b1d6b38194f307864dabc7
BatAmar Battulga <batamar.b@nmtec.co>
2020-05-14 02:35:32 +0800

    sending close emails

--- src/cronJobs/audits.js
+++ src/cronJobs/audits.js
@@ -35,7 +35,39 @@ schedule.scheduleJob('*/1 * * * *', async () => {
     });
   }
 
-  await Audits.closeOpens();
+  // close open audits ===========
+  const closedAuditIds = await Audits.closeOpens();
+  const closedAudits = await Audits.find({ _id: { $in: closedAuditIds } });
+
+  for (const audit of closedAudits) {
+    const responses = await AuditResponses.find({ auditId: audit._id });
+
+    for (const response of responses) {
+      const supplier = await Companies.findOne({ _id: response.supplierId });
+
+      try {
+        await sendEmail({
+          kind: 'supplier__close',
+          toEmails: [supplier.basicInfo.email],
+          audit,
+          supplier,
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+
+    try {
+      await sendEmail({
+        kind: 'buyer__close',
+        toEmails: [MAIN_AUDITOR_EMAIL],
+        audit,
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
   await AuditResponses.disableEditabledResponses();
 
   console.log('Checked audit status'); // eslint-disable-line
